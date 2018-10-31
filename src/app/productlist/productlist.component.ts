bbce5db89815b96335b4207cf566c7221e880113
CodeADH <36961758+CodeADH@users.noreply.github.com>
2018-10-31 22:30:00 +0530

    progress!

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit, Renderer2, AfterViewInit, ViewChild} from '@angular/core';
+import { Component, OnInit, Renderer2, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 import { ProductdataService } from '../productdata.service';
 import { Productlist } from '../models/productlist.model';
 import { NgForm } from '@angular/forms';
 import { cartItem } from '../cartitem';
 import { Product } from '../product';
 import { CartdataService } from '../cartdata.service';
-import { tempVendor } from '../tempVendor'
+import { tempVendor } from '../tempVendor';
 import { viewClassName } from '@angular/compiler';
+import { element } from 'protractor';
+import * as $ from 'jquery'; 
+import { DISABLED } from '@angular/forms/src/model';
+import { style } from '@angular/core/src/animation/dsl';
+
 
 @Component({
   selector: 'app-productlist',
@@ -14,7 +19,12 @@ import { viewClassName } from '@angular/compiler';
   styleUrls: ['./productlist.component.css']
 })
 export class ProductlistComponent implements OnInit {
+  private vendordrop1;
   @ViewChild('addcart') cartbutton1;
+  @ViewChild('viewcart') viewbutton1;
+ // @ViewChild('count') vendordrop1;
+  //const el = this.renderer.selectRootElement('.vendor-drop');
+  //console.log(el);
 
   private productlist;
   vendors= ['Balaji','BigBasket','Other'];
@@ -24,7 +34,8 @@ export class ProductlistComponent implements OnInit {
 
   constructor(private productDataService:ProductdataService,
               private cartDataService:CartdataService,
-              private renderer: Renderer2) { 
+              private renderer: Renderer2,
+              private elem: ElementRef) { 
   }
 
   //model = new Productlist();
@@ -33,13 +44,19 @@ export class ProductlistComponent implements OnInit {
     this.productlist = this.productDataService.getAllProducts();
     // console.log(this.productlist);
     
-    // console.log(this.el.nativeElement.getElementById('cartbutton').disabled);//=true;
+    
   }
 
   ngAfterViewInit(){
-    console.log(this.cartbutton1.nativeElement)
+    //console.log(this.cartbutton1.nativeElement)
     this.cartbutton1.nativeElement.disabled=true;
-  }
+    //console.log(this.vendordrop1.nativeElement)
+    this.viewbutton1.nativeElement.disabled=true;
+    // $(function() {
+    //    $("#mainTable").tableHeadFixer(); 
+    // });
+   // jQuery('#mainTable',).tableHeadFixer();
+    }
 
   submitForm(){
     //console.log(this.cartproduct);
@@ -75,12 +92,17 @@ export class ProductlistComponent implements OnInit {
         }
 
     console.log("reached end of loop" );
+    this.viewbutton1.nativeElement.disabled=false;
+    this.viewbutton1.nativeElement.blink= true;  
+    this.cartbutton1.nativeElement.disabled=true;
+    this.viewbutton1.nativeElement.color='black';
+    this.renderer.setProperty(this.viewbutton1, 'color','black')
     //console.log( this.cartproduct);
-    //delay(10000);
+    
     }
     
 
-  add(id:string, unit:string){
+  add(id:string ){
     //console.log(id);
     //console.log(this.productlist.length);
     for(let i=0;i<this.productlist.length;i++){
@@ -88,11 +110,42 @@ export class ProductlistComponent implements OnInit {
         { 
          // if(this.productlist[i].unit == "Kg")
           this.productlist[i].quantity++;
-          console.log(this.cartbutton1.nativeElement)
-          this.cartbutton1.nativeElement.disabled=false;
-         // this.el.nativeElement.getElementById('cartbutton').disabled=false;
-         // document.getElementById('cartbutton').disabled=false;
+          //console.log(this.vendordrop1.nativeElement);
+          //this.vendordrop1.nativeElement.getElementById[i].disabled=false;
+          let elements = this.elem.nativeElement.querySelectorAll('.vendor-drop');
+          //elements.nativeElement.getElementById(i).disabled=false;
+          //console.log(event.target);
+          //console.log($(event))//.parent().find('.vendor-drop'))//.attr("disabled",true));
+          //console.log($(event).parent().path.chidren(3));
+          let target: object = $(event).find('.vendor-drop')
+          //console.log(target);
+          
+          //$(".vendor-drop").next.css({color:'red'});
+          //console.log(foo);
 
+          //console.log(this..activeElement)
+          //var images:object = 
+          
+          $(".vendor-drop").find("select").get()
+          //prop('enabled');
+          var tura  = $(".vendor-drop").get();
+         let vendordrop1;
+        // @ViewChild('vendor') vendordrop1;
+         console.log(tura[i]);
+          //var arr = images.
+          //console.log(images);
+          this.elem.nativeElement = tura[i];
+          this.elem.nativeElement.disabled=false;
+          
+          
+          //console.log(arr[0]);
+          
+
+          //console.log(document.activeElement);
+          //console.log(elements[i]);
+          //console.log(this.cartbutton1.nativeElement);
+          this.cartbutton1.nativeElement.disabled=false;
+          //getElementsByClassName(vendor-drop).item(0);         
         }
       
     }
@@ -104,38 +157,29 @@ export class ProductlistComponent implements OnInit {
     for(let i=0;i<this.productlist.length;i++){
       if(id === this.productlist[i].id)
         {
-          if(this.productlist[i].quantity>0)
+          if(this.productlist[i].quantity>0){
             this.productlist[i].quantity--; 
-          /*elseif(this.productlist[i].quantity=0)      
-              document.getElementById("cartbutton").disabled=true;*/
+          }
+          
+          if(this.productlist[i].quantity==0){   
+                    var tura = $(".vendor-drop").get();    
+                  //  console.log(tura[i]);
+                    this.elem.nativeElement = tura[i];
+                    this.elem.nativeElement.disabled=true;
+            }
+          }
         }
-    }
+          //document.getElementById("cartbutton").disabled=true;*/
+    
   }
 
    selectedChanged(value:string, count:number) {
     console.log("selectedChanged was called " + value + ' '+ count)
     //console.log(this.productlist[count].id);  
-      let pid:string = this.productlist[count].id;  
-    //  console.log(this.tempVendorArr);
-      this.tempVendorArr.push({pid: pid,vendor: value });
-      console.log(this.tempVendorArr);
-      /*console.log(this.cartproduct.length);
-      for (let i=0;i<this.cartproduct.length;i++){
-
-        if(this.cartproduct[i].product.id === pid){
-          this.cartproduct[i].vendor= value;
-          console.log(this.cartproduct[i].vendor);
-        }
-      }*/
-      
-      //console.log(this.cartDataService.getCart());
-    //console.log(pid + ' '+ this.cartproduct[cartid].vendor);
-      /*
-      console.log (this.productlist[id].trial);
-      let zol:any = this.productDataService.getAllProducts();
-      console.log(zol);*/
-      //console.log(this.cartDataService.getCart);
-    
+          let pid:string = this.productlist[count].id;  
+          this.tempVendorArr.push({pid: pid,vendor: value });
+     // console.log(this.tempVendorArr);
+     
   }
 }
 
